e8f8d231138dddbbf3e6fa3580f1133d0a387861
Fekz115 <fekz115@gmail.com>
2021-02-26 08:31:07 +0300

    Initial commit

---
+++ src/main/java/org/fekz115/innowisetesttask/InnowisetesttaskApplication.java
@@ -0,0 +1,13 @@
+package org.fekz115.innowisetesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowisetesttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnowisetesttaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fekz115/innowisetesttask/InnowisetesttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fekz115.innowisetesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowisetesttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
