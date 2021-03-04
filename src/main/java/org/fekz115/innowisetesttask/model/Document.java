e4d222224981ee28f70541395b02420c378378c0
Fekz115 <fekz115@gmail.com>
2021-03-04 11:58:48 +0300

    model: update to use in db and json

--- src/main/java/org/fekz115/innowisetesttask/model/Document.java
+++ src/main/java/org/fekz115/innowisetesttask/model/Document.java
@@ -1,21 +1,32 @@
 package org.fekz115.innowisetesttask.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Document {
 
+    @Id
+    @GeneratedValue
     int id;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    Set<User> users;
+
     LocalDateTime creationDate;
     String status;
-    Set<User> users;
+    String name;
+
+    @JsonIgnore
     String fileName;
 
 }
--- src/main/java/org/fekz115/innowisetesttask/model/User.java
+++ src/main/java/org/fekz115/innowisetesttask/model/User.java
@@ -1,50 +1,75 @@
 package org.fekz115.innowisetesttask.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Set;
 
+@Entity(name = "usr")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue
     int id;
     String login;
+    @JsonIgnore
     String password;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "UserRole", joinColumns = @JoinColumn(name = "userId"))
+    @JsonIgnore
     Set<Role> roles;
+
+    @ManyToMany
+    @JoinTable(
+            name = "document_users",
+            joinColumns = @JoinColumn(name = "users_id"),
+            inverseJoinColumns = @JoinColumn(name = "document_id")
+    )
+    @JsonIgnore
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     Set<Document> documents;
 
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return getRoles();
     }
 
+    @JsonIgnore
     @Override
     public String getUsername() {
         return getLogin();
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return true;
